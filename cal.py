57b90c1a9bc6db86d6c624f82749268ed214598e
Gspike <agentj1213@gmail.com>
2023-10-10 18:21:10 +0900

    Create a class Fourcal

---
+++ cal.py
@@ -0,0 +1,8 @@
+class FourCal:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+
